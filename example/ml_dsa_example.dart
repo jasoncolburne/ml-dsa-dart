8acfca1047ad257e5c877b805b6acdb7887ac3bb
Jason Colburne <j.colburne@gmail.com>
2025-01-05 02:08:42 -0400

    simple package, no customization

---
+++ example/ml_dsa_example.dart
@@ -0,0 +1,6 @@
+import 'package:ml_dsa/ml_dsa.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/ml_dsa_test.dart
@@ -0,0 +1,16 @@
+import 'package:ml_dsa/ml_dsa.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
