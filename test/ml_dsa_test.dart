0ab102c602a34d3934ced8fded6fdf4e760360d7
Jason Colburne <j.colburne@gmail.com>
2025-01-05 11:03:27 -0400

    tests

--- test/ml_dsa_test.dart
+++ test/ml_dsa_test.dart
@@ -1,22 +1,39 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:ml_dsa/ml_dsa.dart';
 import 'package:test/test.dart';
 
+bool testMLDSARoundTrip(ParameterSet parameters) {
+  final dsa = MLDSA(parameters);
+
+  final message = utf8.encode("hello world");
+  final ctx = Uint8List.fromList([0, 1, 2, 3]);
+
+  final (pk, sk) = dsa.keyGen();
+  final sig = dsa.sign(sk, message, ctx);
+  return dsa.verify(pk, message, sig, ctx);
+}
+
 void main() {
-  group('Round trip', () {
+  group('Round Trip', () {
     setUp(() {
       // Additional setup goes here.
     });
 
     test('ML-DSA-44', () {
-      final _ = MLDSA44Parameters();
+      final params = MLDSA44Parameters();
+      expect(testMLDSARoundTrip(params), true);
     });
 
     test('ML-DSA-65', () {
-      final _ = MLDSA65Parameters();
+      final params = MLDSA65Parameters();
+      expect(testMLDSARoundTrip(params), true);
     });
 
     test('ML-DSA-87', () {
-      final _ = MLDSA87Parameters();
+      final params = MLDSA87Parameters();
+      expect(testMLDSARoundTrip(params), true);
     });
   });
 }
